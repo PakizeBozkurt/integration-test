908948cb2834d0cb1208267ed9dfe47b7e102cf9
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-08 22:57:08 +0100

    passed test

--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -41,6 +41,10 @@ describe("cleanNumbers()", () => {
   });
 
   it("should throw an error if an array with at least one string is provided", () => {
-  
+    const numberValues = ['', 1];
+
+    const cleanFn = () => cleanNumbers(numberValues);
+
+    expect(cleanFn).toThrow();
   });
 });
