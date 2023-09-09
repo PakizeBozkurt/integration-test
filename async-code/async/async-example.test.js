0cd0b6298b4595ac03c6e613ce3fb219ac6e0cb8
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-09 23:03:37 +0100

    fail test

--- async-code/async/async-example.test.js
+++ async-code/async/async-example.test.js
@@ -1,10 +1,17 @@
-import { expect, it } from 'vitest';
-import { generateToken } from './async-example';
+import { expect, it } from "vitest";
+import { generateToken } from "./async-example";
 
-it('should generate a token value', () => {
- const testUserEmail = 'test@example.com';
+it("should generate a token value", (done) => {
+  const testUserEmail = "test@example.com";
 
- generateToken(testUserEmail, (err, token) => {
-     expect(token).toBeDefined();
- });
+  generateToken(testUserEmail, (err, token) => {
+    //  expect(token).toBeDefined();
+
+    try {
+      expect(token).toBe(2);
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
 });
