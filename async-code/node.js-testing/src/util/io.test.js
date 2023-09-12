4c94d9b84f3917d31d58c832a039f43cfd82c9d0
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-12 23:46:49 +0100

    passed test

--- async-code/node.js-testing/src/util/io.test.js
+++ async-code/node.js-testing/src/util/io.test.js
@@ -3,5 +3,7 @@ import { it, expect } from 'vitest';
 import writeData from './io';
 
 it('should execute the writeFile method', () => {
-    
+    const testData = 'Test';
+    const testFilename = 'test.txt';
+    writeData(testData, testFilename);
 });
