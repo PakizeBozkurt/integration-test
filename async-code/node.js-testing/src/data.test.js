6f688d1488ffd60ba414e6857df20f391b2e504a
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-13 22:34:27 +0100

    passed test

--- async-code/node.js-testing/src/data.test.js
+++ async-code/node.js-testing/src/data.test.js
@@ -3,6 +3,9 @@ import { describe, it, expect, vi } from "vitest";
 describe("generateReportData()", () => {
     it('should execute logFn if provided', () => {
         const logger = vi.fn();
+
         generateReportData(logger);
+
+        expect(logger).toBeCalled();
     });
 });
