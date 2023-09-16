c1695a9378352ca24c18d692052335db7151c877
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-16 23:04:00 +0100

    undefined passed

--- async-code/deep-mock/util/errors.test.js
+++ async-code/deep-mock/util/errors.test.js
@@ -1,21 +1,28 @@
 import { describe, expect, it } from "vitest";
 
-import { HttpError } from './errors';
+import { HttpError } from "./errors";
 
-describe('class HttpError', () => {
-    it('should contain the provided status code, message and data', () => {
-        const testStatus = 1;
-        const testMessage = 'Test';
-        const testData = { key: 'test' };
+describe("class HttpError", () => {
+  it("should contain the provided status code, message and data", () => {
+    const testStatus = 1;
+    const testMessage = "Test";
+    const testData = { key: "test" };
 
-        const testError = new HttpError(testStatus, testMessage, testData);
+    const testError = new HttpError(testStatus, testMessage, testData);
 
-        expect(testError.statusCode).toBe(testStatus);
-        expect(testError.message).toBe(testMessage);
-        expect(testError.data).toBe(testData);
-    });
+    expect(testError.statusCode).toBe(testStatus);
+    expect(testError.message).toBe(testMessage);
+    expect(testError.data).toBe(testData);
+  });
 
-    it('should contain undefined as data if no data is provided', () => {
+  it("should contain undefined as data if no data is provided", () => {
+    const testStatus = 1;
+    const testMessage = "Test";
 
-    });
+    const testError = new HttpError(testStatus, testMessage);
+
+    expect(testError.statusCode).toBe(testStatus);
+    expect(testError.message).toBe(testMessage);
+    expect(testError.data).not.toBeDefined;
+  });
 });
