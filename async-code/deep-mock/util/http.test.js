817a7b14275ab432eff20dc35704eb1716269aa3
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-19 21:25:12 +0100

    passed mock global function test

--- async-code/deep-mock/util/http.test.js
+++ async-code/deep-mock/util/http.test.js
@@ -1,27 +1,27 @@
-import { it, vi, expect } from 'vitest';
+import { it, vi, expect } from "vitest";
 
-import { sendDataRequest } from './http';
+import { sendDataRequest } from "./http";
 
-const testResponseData = {testKey: 'testData'};
+const testResponseData = { testKey: "testData" };
 
 const testFetch = vi.fn((url, options) => {
-return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const testResponse = {
-        ok: true,
-        json() {
-            return new Promise((resolve, reject) => {
-                resolve(testResponseData);
-            });
-        }
+      ok: true,
+      json() {
+        return new Promise((resolve, reject) => {
+          resolve(testResponseData);
+        });
+      },
     };
     resolve(testResponse);
+  });
 });
-});
 
-vi.stubGlobal('fetch', testFetch);
+vi.stubGlobal("fetch", testFetch);
 
-it('should return any available response data', () => {
-    const testData = { key: 'test '};
+it("should return any available response data", () => {
+  const testData = { key: "test " };
 
-    return expect(sendDataRequest(testData)). resolves.toEqual(testResponseData);
+  return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
 });
