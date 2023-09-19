88ad57ba9cb66538c913dfff7bd35aafc9605376
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-19 21:19:43 +0100

    sending request

--- async-code/deep-mock/util/http.test.js
+++ async-code/deep-mock/util/http.test.js
@@ -1,5 +1,25 @@
-import { it } from 'vitest';
+import { it, vi } from 'vitest';
+
+import { sendDataRequest } from './http';
+
+const testResponseData = {testKey: 'testData'};
+
+const testFetch = vi.fn((url, options) => {
+return new Promise((resolve, reject) => {
+    const testResponse = {
+        ok: true,
+        json() {
+            return new Promise((resolve, reject) => {
+                resolve(testResponseData);
+            });
+        }
+    };
+    resolve(testResponse);
+});
+});
+
+vi.stubGlobal('fetch', testFetch);
 
 it('should return any available response data', () => {
-    
+    sendDataRequest();
 });
