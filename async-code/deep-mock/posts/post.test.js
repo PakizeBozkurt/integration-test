af40982c06106362a3a4c43c2184d9cc7e11edaf
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-21 22:48:45 +0100

    failed test

--- async-code/deep-mock/posts/post.test.js
+++ async-code/deep-mock/posts/post.test.js
@@ -1,9 +1,22 @@
 import { describe, it } from "vitest";
+import { extractPostData } from "./posts";
 
+describe("extractPostData()", () => {
+  it("should extract title and content from the provided from data", () => {
+    const testTitle = "Test title";
+    const testContent = "Test content";
+    const testFromData = {
+      title: testTitle,
+      content: testContent,
+      get(identifier) {
+        return this[identifier];
+      },
+    };
 
+    const data = extractPostData(testFromData);
 
-describe('extractPostData()', () => {
-    it('should extract title and content from the provided from data', () => {
-        
-    });
+    expect(data.title).toBe(testTitle);
+    expect(data.content).toBe(testContent);
+
+  });
 });
