78006426a192eeb2b22e63bd5df25aa4c10e2a03
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-23 23:20:23 +0100

    error message respected test

--- async-code/deep-mock/util/dom.test.js
+++ async-code/deep-mock/util/dom.test.js
@@ -36,3 +36,7 @@ it('should not contain an error paragraph initially', () => {
 
   expect(errorParagraph).toBeNull;
 });
+
+it("should output the provided message in the error paragraph", () => {
+
+});
